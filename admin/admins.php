8f5ae0c615103df92bd089902f8197ba7ce423e2
hasibjey <hasibjey@gmail.com>
2023-12-22 02:41:03 +0600

    Admin section complate

--- admin/admins.php
+++ admin/admins.php
@@ -1,6 +1,24 @@
 <?php
 $title = "Admins";
 include_once('./inc/header.php');
+$admins = $conn->query("SELECT * FROM `admins`");
+$id = null;
+$name = null;
+$email = null;
+$btnTitle = 'insert';
+
+if (isset($_GET["edit"])) {
+    $eid = $_GET['edit'];
+    $sql = $conn->query("SELECT * FROM `admins` WHERE `id` = '$eid'");
+    if ($sql->num_rows > 0) {
+        while ($admin = mysqli_fetch_assoc($sql)) {
+            $id = $admin['id'];
+            $name = $admin['name'];
+            $email = $admin['email'];
+            $btnTitle = "update";
+        }
+    }
+}
 ?>
 
 <section class="right-section">
@@ -31,31 +49,54 @@ include_once('./inc/header.php');
                         <table class="table table-striped" id="admins-table">
                             <tr>
                                 <th>#</th>
-                                <th>category</th>
-                                <th>slug</th>
-                                <th>status</th>
+                                <th>name</th>
+                                <th>email</th>
                                 <th>Action</th>
                             </tr>
+                            <?php
+                            if ($admins->num_rows > 0) {
+                                $i = 1;
+                                while ($adm = mysqli_fetch_assoc($admins)) {
+                            ?>
+                                    <tr>
+                                        <td><?php echo $i; ?></td>
+                                        <td><?php echo $adm['name']; ?></td>
+                                        <td><?php echo $adm['email']; ?></td>
+                                        <td>
+                                            <div class="d-flex justify-content-between">
+                                                <form action="./controllers/AdminController.php" method="post">
+                                                    <button class="btn btn-sm btn-danger" name="delete" value="<?php echo $adm['id']; ?>"><i class="fa-solid fa-trash"></i></button>
+                                                </form>
+                                                <form action="" method="get">
+                                                    <button class="btn btn-sm btn-success" name="edit" value="<?php echo $adm['id']; ?>"><i class="fa-regular fa-pen-to-square"></i></button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                            <?php }
+                                $i++;
+                            } ?>
                         </table>
                     </div>
                 </div>
             </div>
             <div class="col-4">
                 <div class="card">
-                    <div class="card-header fst-italic">Insert Admins</div>
+                    <div class="card-header fst-italic"><?php echo $btnTitle; ?> Admins</div>
                     <div class="card-body">
                         <form action="./controllers/AdminController.php" method="post">
+                            <input type="text" name="id" value="<?php echo $id ?>" hidden>
                             <div class="mb-3">
                                 <label class="label">name</label>
-                                <input type="text" class="form-control" name="name">
+                                <input type="text" class="form-control" name="name" value="<?php echo $name ?>">
                             </div>
                             <div class="mb-3">
                                 <label class="label">email</label>
-                                <input type="text" class="form-control" name="email">
+                                <input type="text" class="form-control" name="email" value="<?php echo $email ?>">
                             </div>
                             <div class="my-4 text-center">
                                 <button class="btn btn-sm btn-danger px-4" type="reset">Clear</button>
-                                <button class="btn btn-sm btn-success px-4 text-capitalize" type="submit" name="insert">Insert</button>
+                                <button class="btn btn-sm btn-success px-4 text-capitalize" type="submit" name="<?php echo $btnTitle; ?>"><?php echo $btnTitle; ?></button>
                             </div>
                         </form>
                     </div>
--- admin/controllers/AdminController.php
+++ admin/controllers/AdminController.php
@@ -32,37 +32,27 @@ if (isset($_POST["insert"])) {
  */
 if (isset($_POST["update"])) {
     $id = $_POST["id"];
-    $category = $_POST["category"];
-    $slug = $_POST["slug"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
 
-    $sql = $conn->query("UPDATE `categories` SET `category` = '$category', `slug` = '$slug' WHERE `id` = '$id'");
+    $sql = $conn->query("UPDATE `admins` SET `name` = '$name', `email` = '$email' WHERE `id` = '$id'");
 
     if ($sql) {
     ?>
         <script>
-            alert("Category update successfully.");
-            window.location.replace("../category.php");
+            alert("Admin update successfully.");
+            window.location.replace("../admins.php");
         </script>
     <?php
     } else {
     ?>
         <script>
             alert("Opss!");
-            window.location.replace("../category.php");
+            window.location.replace("../admins.php");
         </script>
     <?php
     }
 }
-/**
- * status update
- */
-if (isset($_POST["status"])) {
-    echo $id = $_POST["id"];
-    echo $status = $_POST["status"] == 1 ? 0 : 1;
-    $sql = $conn->query("UPDATE `categories` SET `status` = '$status' WHERE `id` = '$id'");
-    header('Location: ../category.php');
-}
-
 
 /**
  * Delete insert
@@ -70,20 +60,20 @@ if (isset($_POST["status"])) {
 if (isset($_POST["delete"])) {
     $id = $_POST["delete"];
 
-    $sql = $conn->query("DELETE FROM `categories` WHERE `id` = '$id'");
+    $sql = $conn->query("DELETE FROM `admins` WHERE `id` = '$id'");
 
     if ($sql) {
     ?>
         <script>
-            alert("Category delete successfully.");
-            window.location.replace("../category.php");
+            alert("Admin delete successfully.");
+            window.location.replace("../admins.php");
         </script>
     <?php
     } else {
     ?>
         <script>
             alert("Opss!");
-            window.location.replace("../category.php");
+            window.location.replace("../admins.php");
         </script>
 <?php
     }
